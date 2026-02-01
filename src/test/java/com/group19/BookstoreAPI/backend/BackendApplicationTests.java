d9ac2c4c3da55e200d56081716c4ec013eab9a30
johan042711 <157749153+ybI22@users.noreply.github.com>
2026-02-01 04:10:25 -0500

    Initial Spring Boot backend setup

---
+++ src/test/java/com/group19/BookstoreAPI/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group19.BookstoreAPI.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
