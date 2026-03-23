ea0573368c0b4dc7702fc1447d95af447cb08298
Luis Aurelius <40luisr@gmail.com>
2026-03-23 18:22:00 -0400

    test

--- src/main/java/com/group19/BookstoreAPI/backend/entity/Book.java
+++ src/main/java/com/group19/BookstoreAPI/backend/entity/Book.java
@@ -8,7 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+/* Test comment */
 @Entity
 @Table(name = "books")
 public class Book {
